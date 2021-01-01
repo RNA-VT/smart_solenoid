cd0bc50f87bd0897a08be57745d64acf034a0622
Tushar Shah <twoshark@gmail.com>
2021-01-01 14:26:46 -0500

    deps

--- smart_solenoid.ino
+++ smart_solenoid.ino
@@ -2,14 +2,25 @@
 
 #ifdef ESP8266
 #include <esp8266wifi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 #elif defined(ESP32)
 #include <WiFi.h>
+#include <WebServer.h>
 #endif
 
-void setup(){
+#include <ArduinoJson.h>
 
+#define PIN 7
+
+void setup(){
+    pinMode(PIN, OUTPUT)
 }
 
 void loop(){
 
+}
+
+bool getState() {
+    return false;
 }
