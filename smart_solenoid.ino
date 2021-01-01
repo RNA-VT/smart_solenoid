16427335290936c7a9c363ca71bd2f4adb80e71b
Tushar Shah <twoshark@gmail.com>
2021-01-01 14:43:33 -0500

    there can be only 1 wifimanager

--- smart_solenoid.ino
+++ smart_solenoid.ino
@@ -3,13 +3,13 @@
 #ifdef ESP8266
 #include <esp8266wifi.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>
 #elif defined(ESP32)
 #include <WiFi.h>
 #include <WebServer.h>
 #endif
 
 #include <ArduinoJson.h>
+#include <WiFiManager.h>
 
 #define PIN 7
 
